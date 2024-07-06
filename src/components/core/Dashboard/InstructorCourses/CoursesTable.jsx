20e128452e92917b5d59e5482c3888ba20ad9863
SHASHANK2405 <shashankgulati2405@gmail.com>
2024-07-07 01:43:35 +0530

    my courses 2.0

--- src/components/core/Dashboard/InstructorCourses/CoursesTable.jsx
+++ src/components/core/Dashboard/InstructorCourses/CoursesTable.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Table, Tbody, Td, Th, Thead, Tr } from "react-super-responsive-table"
 import { useNavigate } from 'react-router-dom'
 
 export default function CoursesTable({courses , setCourses}) {
@@ -12,7 +13,42 @@ export default function CoursesTable({courses , setCourses}) {
 
   return (
     <div>
-      
+      <Table>
+        <Thead>
+          <Tr className="flex gap-x-10 rounded-t-md border-b border-b-richblack-800 px-6 py-2">
+            <Th className="flex-1 text-left text-sm font-medium uppercase text-richblack-100">
+              Courses
+            </Th>
+            <Th className="text-left text-sm font-medium uppercase text-richblack-100">
+              Duration
+            </Th>
+            <Th className="text-left text-sm font-medium uppercase text-richblack-100">
+              Price
+            </Th>
+            <Th className="text-left text-sm font-medium uppercase text-richblack-100">
+              Actions
+            </Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {
+            courses.length === 0 ? (
+              <Tr>
+                <Td>
+                  No Courses Found
+                </Td>
+              </Tr>
+            )
+            :(
+              courses?.map((course) => {
+                <Tr>
+                  
+                </Tr>
+              })
+            )
+          }
+        </Tbody>
+      </Table>
     </div>
   )
 }
